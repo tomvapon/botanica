dea12c5258795f3770fcfced6fae3d44c1a0ab4c
tomvapon <tomasvarona91@gmail.com>
2023-04-16 17:57:00 +0200

    Use only one column in page

--- src/pages/flores/flores.js
+++ src/pages/flores/flores.js
@@ -10,32 +10,30 @@ function Flores(props) {
             <PageContent data={data} />
             <div className='concept-div'>
                 <h1 className='title'>{flower_info.nombre} ({flower_info.nombre_cientifico})</h1>
-                <div className='row'>
-                    <div className='column'>
-                        <p>{flower_info.descripcion}</p>
-                        <table className='table-bordered'>
-                            <thead className='thead'>
-                                <tr>
-                                    <th scope='col'>CARACTERÍSTICA</th>
-                                    <th scope='col'>VALOR</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Fecha de siembra</td>
-                                    <td>{flower_info.fecha_siembra}</td>
-                                </tr>
-                                <tr>
-                                    <td>Periodo de floración</td>
-                                    <td>{flower_info.fecha_floracion}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <p>Si quieres extender la información sobre {flower_info.nombre} visita el siguiente <a className='extra' href={flower_info.extra}>enlace</a></p>
-                    </div>
-                    <div className='column'>
-                        <img className='treeimage' src={flower_info.image} alt={flower_info.nombre} />
-                    </div>
+                <div className='column'>
+                    <p>{flower_info.descripcion}</p>
+                    <table className='table-bordered'>
+                        <thead className='thead'>
+                            <tr>
+                                <th scope='col'>CARACTERÍSTICA</th>
+                                <th scope='col'>VALOR</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Fecha de siembra</td>
+                                <td>{flower_info.fecha_siembra}</td>
+                            </tr>
+                            <tr>
+                                <td>Periodo de floración</td>
+                                <td>{flower_info.fecha_floracion}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p>Si quieres extender la información sobre {flower_info.nombre} visita el siguiente <a className='extra' href={flower_info.extra}>enlace</a></p>
+                </div>
+                <div className='column'>
+                    <img className='treeimage' src={flower_info.image} alt={flower_info.nombre} />
                 </div>
             </div>
         </div>
